c991c82fc0a542389e70a2515b69ab6770a9179c
sgalera-veridas <sgalera@veridas.com>
2025-07-11 17:37:19 +0200

    Init project with config and first data class

--- app/src/commonMain/kotlin/org/gaztelubira/kmp/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package org.gaztelubira.kmp
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- app/src/commonMain/kotlin/org/gaztelubira/kmp/Platform.kt
+++
@@ -1,7 +0,0 @@
-package org.gaztelubira.kmp
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform

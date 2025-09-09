ddd567fccaa67844eae12dc22710c457feec2d8f
sgaleraalq <sgaleraalq@gmail.com>
2025-09-09 17:45:57 +0200

    Some updates in camera preview

--- domain/src/androidMain/kotlin/com/gbmultiplatform/domain/utils/CameraManager.android.kt
+++ domain/src/androidMain/kotlin/com/gbmultiplatform/domain/utils/CameraManager.android.kt
@@ -20,6 +20,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
 import android.util.Log
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
 import androidx.camera.core.CameraSelector.DEFAULT_FRONT_CAMERA
 import androidx.camera.core.ImageCapture.OnImageCapturedCallback
@@ -29,38 +30,36 @@ import androidx.camera.view.CameraController.IMAGE_CAPTURE
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Arrangement.SpaceAround
+import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells.Fixed
 import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.Icons.Default
+import androidx.compose.material.icons.filled.Camera
 import androidx.compose.material.icons.filled.Cameraswitch
-import androidx.compose.material.icons.filled.Photo
-import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -70,7 +69,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
 // CameraManager.android.kt
 @OptIn(ExperimentalMaterial3Api::class)
@@ -79,13 +77,24 @@ actual fun CameraManagerCompose(
     onResult: (CommonImage?) -> Unit
 ) {
     val viewModel = remember { CameraViewModel() }
-    val scope = rememberCoroutineScope()
     val scaffoldState = rememberBottomSheetScaffoldState()
     val context = LocalContext.current
     val controller = remember {
         LifecycleCameraController(context).apply { setEnabledUseCases(IMAGE_CAPTURE) }
     }
     val bitmaps by viewModel.bitmaps.collectAsState()
+
+    fun isBackCamera(): Boolean {
+        return controller.cameraSelector == DEFAULT_BACK_CAMERA
+    }
+
+    fun changeCamera(): CameraSelector {
+        return if (isBackCamera()) {
+            DEFAULT_FRONT_CAMERA
+        } else {
+            DEFAULT_BACK_CAMERA
+        }
+    }
 
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
@@ -110,40 +119,31 @@ actual fun CameraManagerCompose(
             )
 
             IconButton(
+                modifier = Modifier.offset(16.dp, 16.dp),
+                colors = IconButtonDefaults.iconButtonColors(
+                    containerColor = White
+                ),
                 onClick = {
-                    controller.cameraSelector =
-                        if (controller.cameraSelector == DEFAULT_BACK_CAMERA) {
-                            DEFAULT_FRONT_CAMERA
-                        } else DEFAULT_BACK_CAMERA
-                },
-                modifier = Modifier.offset(16.dp, 16.dp)
+                    controller.cameraSelector = changeCamera()
+                }
             ) {
                 Icon(
-                    imageVector = Icons.Default.Cameraswitch,
+                    imageVector = Default.Cameraswitch,
                     contentDescription = "Switch camera"
                 )
             }
 
-            Row(
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .align(BottomCenter)
                     .padding(16.dp),
-                horizontalArrangement = SpaceAround
+                contentAlignment = Center
             ) {
                 IconButton(
-                    onClick = {
-                        scope.launch {
-                            scaffoldState.bottomSheetState.expand()
-                        }
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Default.Photo,
-                        contentDescription = "Open gallery"
-                    )
-                }
-                IconButton(
+                    colors = IconButtonDefaults.iconButtonColors(
+                        containerColor = White
+                    ),
                     onClick = {
                         takePhoto(
                             controller = controller,
@@ -153,8 +153,8 @@ actual fun CameraManagerCompose(
                     }
                 ) {
                     Icon(
-                        imageVector = Icons.Default.PhotoCamera,
-                        contentDescription = "Take photo"
+                        imageVector = Default.Camera,
+                        contentDescription = "Take picture"
                     )
                 }
             }
@@ -247,8 +247,8 @@ fun PhotoBottomSheetContent(
         }
     } else {
         LazyVerticalStaggeredGrid(
-            columns = StaggeredGridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            columns = Fixed(2),
+            horizontalArrangement = spacedBy(16.dp),
             verticalItemSpacing = 16.dp,
             contentPadding = PaddingValues(16.dp),
             modifier = modifier
